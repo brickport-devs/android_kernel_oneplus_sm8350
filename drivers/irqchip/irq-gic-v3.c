a7e0baf7f2b0e1c2acb548fafc61c01130576869
atndko <z1281552865@gmail.com>
2021-09-19 06:55:51 +0800

    Revert "ARM: gic-v3: Log the IRQs in RTB before handling an IRQ"
    This reverts commit 23c2a9291f25e5ee26f9bdb1df8d50d5e8a411c9.
    
    Signed-off-by: atndko <z1281552865@gmail.com>

--- drivers/irqchip/irq-gic-v3.c
+++ drivers/irqchip/irq-gic-v3.c
@@ -19,7 +19,6 @@
 #include <linux/refcount.h>
 #include <linux/slab.h>
 #include <linux/suspend.h>
-#include <linux/msm_rtb.h>
 #include <linux/wakeup_reason.h>
 
 
@@ -715,7 +714,6 @@ static asmlinkage void __exception_irq_entry gic_handle_irq(struct pt_regs *regs
 	if (likely(irqnr > 15)) {
 		int err;
 
-		uncached_logk(LOGK_IRQ, (void *)(uintptr_t)irqnr);
 		if (static_branch_likely(&supports_deactivate_key))
 			gic_write_eoir(irqnr);
 		else
@@ -732,7 +730,6 @@ static asmlinkage void __exception_irq_entry gic_handle_irq(struct pt_regs *regs
 		return;
 	}
 	if (irqnr < 16) {
-		uncached_logk(LOGK_IRQ, (void *)(uintptr_t)irqnr);
 		gic_write_eoir(irqnr);
 		if (static_branch_likely(&supports_deactivate_key))
 			gic_write_dir(irqnr);

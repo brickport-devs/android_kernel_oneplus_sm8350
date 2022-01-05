be0fb1daa5a189478aa813a423e86f5a863ee65a
atndko <z1281552865@gmail.com>
2022-01-05 19:48:29 +0800

    exec: Add init.kernel.post_boot-lahaina.sh to tampering blacklist
    * OOS 11 uses init.kernel.post_boot-lahaina.sh to adjust parameters.
    
    Signed-off-by: atndko <z1281552865@gmail.com>

--- include/linux/binfmts.h
+++ include/linux/binfmts.h
@@ -164,7 +164,8 @@ static inline bool task_is_booster(struct task_struct *tsk)
 	       !strcmp(comm, "power@1.3-servi") ||
 	       !strcmp(comm, "perf@1.0-servic") ||
 	       !strcmp(comm, "perf@2.0-servic") ||
-		   !strcmp(comm, "perf@2.2-servic") ||
+	       !strcmp(comm, "perf@2.2-servic") ||
+	       !strcmp(comm, "it.kernel.post_") ||
 	       !strcmp(comm, "init.qcom.post_");
 }
 

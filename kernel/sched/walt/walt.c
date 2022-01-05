ec8ecc5f3a3ad311e35c779d390bbdb561d60e91
atndko <z1281552865@gmail.com>
2022-01-05 20:13:43 +0800

    Revert "sched/walt: Never allow userspace affect load scale"
    This reverts commit 680d5c54511f5adc814cf4fb209498ac26cb0ab3.
    
    Signed-off-by: atndko <z1281552865@gmail.com>

--- kernel/sched/walt/walt.c
+++ kernel/sched/walt/walt.c
@@ -3811,8 +3811,6 @@ int walt_proc_user_hint_handler(struct ctl_table *table,
 	unsigned int old_value;
 	static DEFINE_MUTEX(mutex);
 
-	return 0;
-
 	mutex_lock(&mutex);
 
 	old_value = sysctl_sched_user_hint;
